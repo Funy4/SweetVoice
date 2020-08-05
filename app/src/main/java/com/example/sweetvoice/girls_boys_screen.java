f719af3b40dbbcc77c3cde97b698ddc0646f0794
Eugene <jenya.kr.2001@gmail.com>
2020-08-05 12:07:31 +0300

    first comm

--- app/src/main/java/com/example/sweetvoice/girls_boys_screen.java
+++ app/src/main/java/com/example/sweetvoice/girls_boys_screen.java
@@ -2,14 +2,49 @@ package com.example.sweetvoice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class girls_boys_screen extends AppCompatActivity {
 
+    private ImageView boy, girl;
+    private String preferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_girls_boys_screen);
         getSupportActionBar().hide();
+        Listener();
     }
+    public void Listener(){
+        boy = (ImageView)findViewById(R.id.imgBoy);
+        girl = (ImageView)findViewById(R.id.imgGirl);
+
+        boy.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        preferences = "Boy";
+                        Intent intent = new Intent(girls_boys_screen.this, age_screen.class);
+                        startActivity(intent);
+                        finish();
+
+                    }
+            }
+        );
+        girl.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        preferences = "Girl";
+                        Intent intent = new Intent(girls_boys_screen.this, age_screen.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }
+        );
+    }
+
 }
---
+++ app/src/main/java/com/example/sweetvoice/wait_screen.java
@@ -0,0 +1,14 @@
+package com.example.sweetvoice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class wait_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wait_screen);
+    }
+}
