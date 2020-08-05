a0bf924231daf3905d1d37be44af7f4799b5c0f9
Eugene <jenya.kr.2001@gmail.com>
2020-08-05 11:37:00 +0300

    logic

--- app/src/main/java/com/example/sweetvoice/Login_screen.java
+++ app/src/main/java/com/example/sweetvoice/Login_screen.java
@@ -6,13 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Login_screen extends AppCompatActivity {
 
 
     private TextView forgot;
     private Button btn;
+    private EditText log, pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,9 @@ public class Login_screen extends AppCompatActivity {
     public void Listener(){
         forgot = (TextView)findViewById(R.id.forgTxt);
         btn = (Button)findViewById(R.id.btnNext);
+        log = (EditText)findViewById(R.id.txtLog);
+        pass = (EditText)findViewById(R.id.txtPass);
+
 
         forgot.setOnClickListener(
                 new View.OnClickListener() {
@@ -39,9 +45,14 @@ public class Login_screen extends AppCompatActivity {
                 new View.OnClickListener(){
                     @Override
                     public void onClick(View view){
-                        Intent intent = new Intent(Login_screen.this, girls_boys_screen.class);
-                        startActivity(intent);
-                        finish();
+                        if (log.getText().length() > 0 && pass.getText().length() > 0) {
+                            Intent intent = new Intent(Login_screen.this, girls_boys_screen.class);
+                            startActivity(intent);
+                            finish();
+                        }else
+                            Toast.makeText(Login_screen.this,
+                                    "Неправильный логин или пароль",
+                                    Toast.LENGTH_SHORT).show();
                     }
                 }
         );
--- app/src/main/java/com/example/sweetvoice/new_password.java
+++ app/src/main/java/com/example/sweetvoice/new_password.java
@@ -2,15 +2,49 @@ package com.example.sweetvoice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class new_password extends AppCompatActivity {
+
+    private Button btn;
+    private EditText log, pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_password);
         getSupportActionBar().hide();
+        Listenner();
+    }
+    public void Listenner(){
+        btn = (Button)findViewById(R.id.btnNext);
+        log = (EditText)findViewById(R.id.txtLog);
+        pass = (EditText)findViewById(R.id.txtPass);
 
+        btn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (log.equals(pass) && log.length() > 0 && pass.length() > 0){
+                            Intent intent = new Intent(new_password.this, girls_boys_screen.class);
+                            startActivity(intent);
+                            finish();
+                        }else{
+                            Toast.makeText(new_password.this,
+                                    "Поля не совпадают",
+                                    Toast.LENGTH_SHORT
+                            ).show();
+
+                            log.setText("");
+                            pass.setText("");
+                        }
+                    }
+                }
+        );
     }
 }
